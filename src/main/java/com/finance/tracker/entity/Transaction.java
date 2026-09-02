00765c77470e5993016bf09c227ebd4233dabfdd
Tourist0098 <jagadishpratap4@gmail.com>
2026-09-02 11:56:14 +0530

    chore: rewrite entity class for muscle memory

--- src/main/java/com/finance/tracker/entity/Transaction.java
+++ src/main/java/com/finance/tracker/entity/Transaction.java
@@ -1,9 +1,6 @@
-package com.finance.tracker.entity;
+package com.finance.tracker.entity; 
 import java.time.LocalDate;
 
-import com.finance.tracker.TransactionType;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -12,53 +9,47 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+
 @Entity
 @Table(name = "Transactions")
-public class Transaction {
+public class Transaction{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private TransactionType type;
     private double amount;
     private String category;
     private LocalDate date;
 
-    public Transaction(){}
-    public Transaction(TransactionType type, double amount, String category, LocalDate date){
-        this.type = type;
-        this.amount = amount;
-        this.category = category;
-        this.date = date;
-    }
-
-    //GETTERS and SETTTERS
+    //GETTERS
     public Long getId(){
         return id;
     }
     public TransactionType getType(){
         return type;
     }
-    public void setType(TransactionType type){
-        this.type = type;
-    }
     public double getAmount(){
         return amount;
-    }
-    public void setAmount(double amnt){
-        this.amount = amnt;
     }
     public String getCategory(){
         return category;
     }
-    public void setCategory(String category){
-        this.category = category;
-    }
     public LocalDate getDate(){
         return date;
     }
-    public void setCategory(LocalDate date){
-        this.date = date;
+
+    //SETTERS
+    public void setType(TransactionType t){
+        this.type = t;
+    }
+    public void setAmount(double a){
+        this.amount = a;
+    }
+    public void setCategory(String c){
+        this.category = c;
+    }
+    public void setDate(LocalDate ld){
+        this.date = ld;
     }
 }
